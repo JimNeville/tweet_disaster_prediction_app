19e4394169735f1269be057dc4e4bafca831d4a4
Jim Neville <jrneville@gmail.com>
2020-03-24 23:12:44 -0600

    update test client and add wordnet download

--- test_client.py
+++ test_client.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
-port = 3000
-url = 'http://localhost:' + str(port) + '/predict.json'
+#port = 3000
+#url = 'http://localhost:' + str(port) + '/predict.json'
+
+url = 'https://tweet-disaster-prediction.herokuapp.com/predict.json'
 
 test_tweet = 'This is a test'
 tweet_json = json.dumps({"text": test_tweet})
