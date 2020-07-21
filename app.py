04c7687c70b41697ee018818608fb79c3be3553b
Jim Neville <jrneville@gmail.com>
2020-07-20 21:30:58 -0600

    added json endpoint back

--- app.py
+++ app.py
@@ -42,10 +42,26 @@ class predict(Resource):
 			return make_response(render_template('prediction_form.html', prediction = 'Error', text = text_dict['text']), 200, headers)
 
 
+class predict_json(Resource):
+	def post(self):
+		text_dict = request.get_json(force=True)
+		text_df = pd.DataFrame([text_dict['text']], columns = ['text'], index = [0])
+		pre.preprocess_text_cols(text_df)
+		pre.create_new_features(text_df)
+
+		global corpus
+		word_features_train_df = pre.tf_idf(text_df, 'final_text', corpus)
+		train, features = pre.return_final_df(text_df, word_features_train_df, target_series=None)
+
+		prediction = int(model.predict(train[features])[0])
+
+		return jsonify({"result": prediction})
+
 # Endpoints
 
 api.add_resource(status, "/status")
 api.add_resource(predict, "/predict")
+api.add_resource(predict_json, "/predict.json")
 
 # Run app
 
